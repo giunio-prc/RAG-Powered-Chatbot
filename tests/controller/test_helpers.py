768584073c8efeaddb50c17e6bbccb2adca881bf
Giunio <giuniodl@live.it>
2025-06-09 14:15:01 +0200

    introduce faker

--- tests/controller/test_helpers.py
+++ tests/controller/test_helpers.py
@@ -4,12 +4,10 @@ from app.controller.helpers import chunk_document
 
 
 @pytest.fixture
-def document():
-    return """
-Lorem Ipsum
-"""
+def paragraph(faker):
+    return faker.paragraph(nb_sentences=10)
 
 
-def test_a_document__can_be_reduced_in_chunks(document):
-    chunks = chunk_document(document)
+def test_a_document__can_be_reduced_in_chunks(paragraph):
+    chunks = chunk_document(paragraph)
     assert False
