a5654213e6eccd3480834b4c495ab3b245e98bb4
Giunio <giuniodl@live.it>
2025-07-07 14:13:44 +0200

    rearrange type hints for consistency

--- app/adapters/fake_database.py
+++ app/adapters/fake_database.py
@@ -5,7 +5,7 @@ from app.interfaces import DatabaseManagerInterface
 
 
 class FakeDatabase(DatabaseManagerInterface):
-    db: list[str]
+    db: list[Document]
 
     def __init__(self):
         self.db = []
@@ -14,20 +14,22 @@ class FakeDatabase(DatabaseManagerInterface):
         )
 
     async def add_chunks(self, chunks: list[Document]):
-        self.db.extend([document.page_content for document in chunks])
+        self.db.extend(chunks)
 
     async def get_context(self, question) -> str:
-        return " ".join(self.db)
+        return "\n\n".join([document.page_content for document in self.db])
 
     async def add_text_to_db(self, text: str):
         chunks = self.text_splitter.split_text(text)
-        self.db.extend(chunks)
+        self.db.extend([Document(page_content=chunk) for chunk in chunks])
 
-    def get_chunks(self) -> list[str]:
+    def get_chunks(self) -> list[Document]:
         return self.db
 
     def get_number_of_vectors(self) -> int:
         return len(self.db)
 
     def get_length_of_longest_vector(self) -> int:
-        return len(max(self.db, key=len))
+        if not self.db:
+            return 0
+        return len(max(self.db, key=lambda x: len(x.page_content)))
