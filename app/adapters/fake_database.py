139a6a5aa6b22cb12700dd9a48893fdea7770d26
Giunio <giuniodl@live.it>
2025-07-20 09:39:04 +0200

    remove document object fom fake database

--- app/adapters/fake_database.py
+++ app/adapters/fake_database.py
@@ -2,13 +2,12 @@ from os import PathLike
 
 from langchain.text_splitter import CharacterTextSplitter
 from langchain_community.document_loaders import DirectoryLoader
-from langchain_core.documents.base import Document
 
 from app.interfaces import DatabaseManagerInterface
 
 
 class FakeDatabase(DatabaseManagerInterface):
-    db: list[Document]
+    db: list[str]
 
     text_splitter: CharacterTextSplitter = CharacterTextSplitter(
         chunk_size=200, chunk_overlap=0, separator="\n"
@@ -18,17 +17,17 @@ class FakeDatabase(DatabaseManagerInterface):
         self.db = []
 
     async def add_chunks(self, chunks: list[str]):
-        self.db.extend([Document(page_content=chunk) for chunk in chunks])
+        self.db.extend(chunks)
 
     async def get_context(self, question) -> str:
-        return "\n\n".join([document.page_content for document in self.db])
+        return "\n\n".join(self.db)
 
     async def add_text_to_db(self, text: str):
         chunks = self.text_splitter.split_text(text)
-        self.db.extend([Document(page_content=chunk) for chunk in chunks])
+        self.db.extend(chunks)
 
     def get_chunks(self) -> list[str]:
-        return [document.page_content for document in self.db]
+        return self.db
 
     def get_number_of_vectors(self) -> int:
         return len(self.db)
@@ -36,11 +35,14 @@ class FakeDatabase(DatabaseManagerInterface):
     def get_length_of_longest_vector(self) -> int:
         if not self.db:
             return 0
-        return len(max(self.db, key=lambda x: len(x.page_content)))
+        return len(max(self.db, key=len))
 
     def empty_database(self):
         self.db = []
 
     def load_documents_from_folder(self, folder: PathLike):
         documents = DirectoryLoader(str(folder), "*.txt").load()
-        self.db.extend(self.text_splitter.split_documents(documents))
+        self.db.extend(
+            document.page_content
+            for document in self.text_splitter.split_documents(documents)
+        )
