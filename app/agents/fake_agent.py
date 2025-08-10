270eed79a3a08891dc43c14cce2bc2d965a96a04
Giunio <giuniodl@live.it>
2025-08-10 09:49:17 +0200

    improve fake agent

--- app/agents/fake_agent.py
+++ app/agents/fake_agent.py
@@ -1,9 +1,12 @@
+from asyncio import sleep
 from collections.abc import AsyncIterator
+from random import random
 
 from app.interfaces.agent import AIAgentInterface
 
 
 class FakeAgent(AIAgentInterface):
+
     async def query_with_context(self, question: str, context: str) -> str:
         return (
             f'You asked me the following question:\n "{question}" \n'
@@ -15,10 +18,28 @@ class FakeAgent(AIAgentInterface):
         self, question: str, context: str
         ) -> AsyncIterator[str]:
 
-        yield "You asked me the following question:\n"
+        char_sleep = 5.0 / len(f"{context}{question}")
+        phrase_sleep = char_sleep * 2
 
-        yield f'"{question}" \n'
+        for char in "You asked me the following question:\n":
+            await sleep(char_sleep * random())
+            yield char
 
-        yield f'With the following context "{context}"\n'
+        await sleep(phrase_sleep * random())
 
-        yield "Unfortunately I am a fake agent I am not able to answer you"
+        for char in f'"{question}" \n':
+            await sleep(char_sleep * random())
+            yield char
+
+        await sleep(phrase_sleep * random())
+
+
+        for char in f'With the following context "{context}"\n':
+            await sleep(char_sleep * random())
+            yield char
+
+        await sleep(phrase_sleep * random())
+
+        for char in "Unfortunately I am a fake agent I am not able to answer you":
+            await sleep(char_sleep * random())
+            yield char
