c14e4aec4beef04f59f66d301edeae5bd347d881
Giunio <giuniodl@live.it>
2025-08-12 15:03:13 +0200

    adjust conditional skip

--- tests/conftest.py
+++ tests/conftest.py
@@ -6,8 +6,10 @@ import pytest
 from app.agents.fake_agent import FakeAgent
 from app.databases.fake_database import FakeDatabase
 
+missing_cohere_key_in_env = "COHERE_API_KEY" not in os.environ,
+
 skip_due_to_cohere_api_key = pytest.mark.skipif(
-    "COHERE_API_KEY" not in os.environ,
+    missing_cohere_key_in_env,
     reason="COHERE_API_KEY required"
 )
 
@@ -18,7 +20,10 @@ def fake_database() -> Generator[FakeDatabase]:
     database.empty_database()
 
 @pytest.fixture
+@skip_due_to_cohere_api_key
 def chroma_database() -> Generator["_ChromaDatabase"]:
+    if missing_cohere_key_in_env:
+        pytest.skip()
     from app.databases import ChromaDatabase as _ChromaDatabase  # type: ignore
     database = _ChromaDatabase()
     yield database
@@ -30,7 +35,10 @@ def fake_agent() -> FakeAgent:
     return agent
 
 @pytest.fixture
+@skip_due_to_cohere_api_key
 def cohere_agent() -> "_CohereAgent":
+    if missing_cohere_key_in_env:
+        pytest.skip()
     from app.agents import CohereAgent as _CohereAgent  # type: ignore
     agent = _CohereAgent()
     return agent
