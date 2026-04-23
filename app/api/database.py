96741d8ecf682d1b964ceef8edfac7debde66381
Giunio <giuniodl@live.it>
2026-04-23 11:42:51 +0200

    add empty database button

--- app/api/database.py
+++ app/api/database.py
@@ -42,3 +42,12 @@ async def get_vectors_data(
     number_of_vectors = db.get_number_of_vectors(cookie_session)
     longest_vector = db.get_length_of_longest_vector(cookie_session)
     return {"number_of_vectors": number_of_vectors, "longest_vector": longest_vector}
+
+
+@router.delete("/empty-database")
+async def empty_database(
+    db: get_db_from_state_annotation,
+    cookie_session: Annotated[str, Depends(get_cookie_session)],
+):
+    db.empty_database(cookie_session)
+    return {"message": "Database emptied successfully"}
